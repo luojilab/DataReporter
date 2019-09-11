8dc548e2a37df96c35a61a6df79bb3018f1d9b47
lixiaoyu <lixiaoyu@luojilab.com>
2019-09-11 20:24:53 +0800

    fix(all):perfect build script

--- build/iOS/build_os_arch.sh
+++ build/iOS/build_os_arch.sh
@@ -2,6 +2,6 @@
 mkdir -p build/os
 mkdir -p libs/os
 cd build/os
-cmake -DDEBUG=OFF -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DIOS_PLATFORM=OS ../../../../src
+cmake -DDEBUG=OFF -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DIOS_PLATFORM=OS ../
 make
 mv libdata-reporter.a ../../libs/os/
