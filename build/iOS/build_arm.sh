52846716f88b381c62bbd821c8047b10988028e2
lixiaoyu <lixiaoyu0123@163.com>
2022-01-27 22:29:22 +0800

    fix(all):release iOS

--- build/iOS/build_arm.sh
+++ build/iOS/build_arm.sh
@@ -3,6 +3,6 @@ rm -rf build/arm
 mkdir -p build/arm
 mkdir -p libs/arm
 cd build/arm
-cmake -DDEBUG=OFF -DNDEBUG=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DENABLE_ARC=1 -DPLATFORM=OS ../../
+cmake -DDEBUG=OFF -DNDEBUG=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DDEPLOYMENT_TARGET="8.0" -DENABLE_ARC=1 -DPLATFORM=OS ../../
 make -j4
 mv -f DataReporter.framework ../../libs/arm/
